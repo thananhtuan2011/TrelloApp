0e5c773c6c83b28189419fa5060674bc20d883dd
gj-tuanta <tuan.than@bstarsolutions.com>
2024-08-19 10:14:27 +0700

    update

--- trello-web/src/App.jsx
+++ trello-web/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React ++</h1>
+      <Button variant="contained">Hello world</Button>
+      <div className='font-bold underline'>Test tailwindcss</div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
