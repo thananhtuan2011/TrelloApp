7c4be41698fafd82a42e737a613301be45b0c364
Anh Tuan <thananhtuan2011@gmail.com>
2024-09-22 20:51:15 +0700

    up

--- Trello-api-by-me/src/routes/v1/boardRoute.js
+++ Trello-api-by-me/src/routes/v1/boardRoute.js
@@ -2,14 +2,14 @@ import express from 'express'
 import { StatusCodes } from 'http-status-codes';
 import { boarController } from '~/controllers/boardController';
 import { BoardValidation } from '~/validations/boardValidation';
-const router = express.Router();
+const Router = express.Router();
 
-router.get('/getboard', (req, res) => {
+Router.get('/getboard', (req, res) => {
     res.status(StatusCodes.OK).json({ message: "ok getboard sss" })
     // res.send("ok getboard sss ")
 })
 //  created board validation and controller
-router.post('/created', BoardValidation.CreatedNewBoard, boarController.CreatedNewBoardController)
+Router.post('/created', BoardValidation.CreatedNewBoard, boarController.CreatedNewBoardController)
 
 export default Router
 // export  const Board_router =router;
